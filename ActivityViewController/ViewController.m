14f231295925094b3091ae2d3614a916de816fc1
Arepu Pavankumar <iosdeveloper.ipa@gmail.com>
2016-02-22 00:08:17 +0530

    UIActivityViewController
    Usage of UIActivityViewController - How to share your thought/files to
    any one from your apps

--- ActivityViewController/ViewController.m
+++ ActivityViewController/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIActivityViewController *activityVC;
+}
+@property (weak, nonatomic) IBOutlet UITextView *myTextView;
+- (IBAction)showActivity:(id)sender;
 @end
 
 @implementation ViewController
@@ -19,9 +23,36 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+   //
+//    let activityViewController = UIActivityViewController(activityItems: [self.noteTextview.text], applicationActivities: nil)
+//    
+//    activityViewController.excludedActivityTypes = [UIActivityTypeMail]
+//    
+//    self.presentViewController(activityViewController, animated: true, completion: nil)
+    
+    
 }
 
+- (IBAction)showActivity:(id)sender
+{
+
+    
+    NSArray *myArray = [[NSArray alloc]initWithObjects:self.myTextView.text, nil];
+    
+   // NSArray *types = [[NSArray alloc]initWithObjects:UIActivityTypeMail, nil];
+    
+    // Dispose of any resources that can be recreated.
+    activityVC = [[UIActivityViewController alloc]initWithActivityItems:myArray applicationActivities:nil];
+    
+   // activityVC.excludedActivityTypes = types;
+    
+    [self presentViewController:activityVC animated:YES completion:nil];
+    
+    
+
+}
 @end
